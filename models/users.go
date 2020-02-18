95084382d0433a0feabcfcdce7099122e1f033e4
Andrew Brudnak <brudnak@icloud.com>
2020-02-18 15:43:55 -0700

    :art: Adding import change

--- models/users.go
+++ models/users.go
@@ -1,7 +1,9 @@
 package models
 
-import "github.com/jinzhu/gorm"
-
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
 type User struct {
 	gorm.Model
 	Name string
