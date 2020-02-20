9f2185fc44029875b380b989eeb255249d8c2222
Andrew Brudnak <brudnak@icloud.com>
2020-02-19 21:49:16 -0700

    :books: Added in some comments

--- models/users.go
+++ models/users.go
@@ -59,6 +59,9 @@ func (us *UserService) ByEmail(email string) (*User, error) {
 	return &user, err
 }
 
+// first will query using the provided gorm.DB and it will
+// get the first item returned and place it into dst. If
+// nothing is found in the query, it will return ErrNotFound
 func first(db *gorm.DB, user *User) error {
 	err := db.First(user).Error
 	if err == gorm.ErrRecordNotFound {
