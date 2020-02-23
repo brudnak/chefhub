fe0b44f3b6154d71405ef49d13230540e95d2489
Andrew Brudnak <brudnak@icloud.com>
2020-02-23 00:37:03 -0700

    :shipit: Cleaning up users model

--- models/users.go
+++ models/users.go
@@ -165,15 +165,7 @@ func (uv *userValidator) ByRemember(token string) (*User, error) {
 // Create will create the provided user and back fill data
 // like the ID, CreatedAt, and UpdatedAt fields.
 func (uv *userValidator) Create(user *User) error {
-	if user.Remember == "" {
-		token, err := rand.RememberToken()
-		if err != nil {
-			return err
-		}
-		user.Remember = token
-	}
-
-	err := runUserValFuncs(user, uv.bcryptPassword, uv.hmacRemember)
+	err := runUserValFuncs(user, uv.bcryptPassword, uv.setRememberIfUnset, uv.hmacRemember)
 	if err != nil {
 		return err
 	}
@@ -212,6 +204,20 @@ func (uv *userValidator) bcryptPassword(user *User) error {
 	user.PasswordHash = string(hashedBytes)
 	user.Password = ""
 	return nil
+}
+
+func (uv *userValidator) setRememberIfUnset(user *User) error {
+	if user.Remember != "" {
+		return nil
+	}
+
+	token, err := rand.RememberToken()
+	if err != nil {
+		return err
+	}
+	user.Remember = token
+	return nil
+
 }
 
 func (uv *userValidator) hmacRemember(user *User) error {
