004f112056bb241ca4963f726e218a0ec791c387
Andrew Brudnak <brudnak@icloud.com>
2020-02-23 21:28:11 -0700

    :apple: Started the shared services
    :neckbeard: unrelated info beyond this point :sparkles:
    
    :shipit:
    
    where to find me
    
    • :bird: https://twitter.com/Andrew_Brudnak
    • :bowtie: https://www.linkedin.com/in/andrew-brudnak/
    • :fox_face: https://gitlab.com/brudnak
    • :sake: https://bitbucket.org/brudnak/
    • :octocat: https://github.com/brudnak
    
    :blue_heart: just an aspiring gopher
    
    :lock: every commit i've ever made has been
    been signed with a gpg key
    
    :briefcase:
    https://www.midori-global.com/blog/2018/04/02/git-50-72-rule
    
    :anchor: have followed the 50/72 rule from day one
    
    :blue_heart: Main Go site
    https://go.dev/
    
    :blue_heart: Go Proverbs
    https://go-proverbs.github.io/
    
    :blue_heart: Zen of Go
    https://dave.cheney.net/2020/02/23/the-zen-of-go
    
    :sparkling_heart: Coding non-stop for Russell & Ruby
    :two_hearts: Ruby Genevieve
    :two_hearts: Russell James
    
    • :pray: bless the youtube algorithm
    • :headphones: https://youtu.be/waAlgFq9Xq8
    • :earth_americas: truth
    • :milky_way: https://youtu.be/9D05ej8u-gU

--- models/galleries.go
+++ models/galleries.go
@@ -9,3 +9,10 @@ type Gallery struct {
 	UserID uint   `gorm:"noy_null;index"`
 	Title  string `gorm:"not_null"`
 }
+
+// GalleryService is our interface.
+type GalleryService interface{}
+
+// GalleryDB interface for our data connection
+type GalleryDB interface {
+}
