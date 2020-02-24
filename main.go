14982945e659462788adf049845caa0801e1dc1b
Andrew Brudnak <brudnak@icloud.com>
2020-02-23 22:48:56 -0700

    :fire: Moving close to the Services type
    Moved the close and migrate functions to the
    services type.
    
    :neckbeard: unrelated info beyond this point :sparkles:
    
    :shipit:
    
    where to find me
    
    • :bird: https://twitter.com/Andrew_Brudnak
    • :bowtie: https://www.linkedin.com/in/andrew-brudnak/
    • :fox_face: https://gitlab.com/brudnak
    • :sake: https://bitbucket.org/brudnak/
    • :octocat: https://github.com/brudnak
    
    :blue_heart: just an aspiring gopher
    
    :lock: every commit i've ever made has been
    been signed with a gpg key
    
    :briefcase:
    https://www.midori-global.com/blog/2018/04/02/git-50-72-rule
    
    :anchor: have followed the 50/72 rule from day one
    
    :blue_heart: Main Go site
    https://go.dev/
    
    :blue_heart: Go Proverbs
    https://go-proverbs.github.io/
    
    :blue_heart: Zen of Go
    https://dave.cheney.net/2020/02/23/the-zen-of-go
    
    :sparkling_heart: Coding non-stop for Russell & Ruby
    :two_hearts: Ruby Genevieve
    :two_hearts: Russell James
    
    • :pray: bless the youtube algorithm
    • :headphones: https://youtu.be/waAlgFq9Xq8
    • :earth_americas: truth
    • :milky_way: https://youtu.be/9D05ej8u-gU

--- main.go
+++ main.go
@@ -22,9 +22,8 @@ func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	services, err := models.NewServices(psqlInfo)
 	must(err)
-	// TODO: Fix this
-	// defer us.Close()
-	// us.AutoMigrate()
+	defer services.Close()
+	services.AutoMigrate()
 
 	staticC := controllers.NewStatic()
 	usersC := controllers.NewUsers(services.User)
--- models/services.go
+++ models/services.go
@@ -2,6 +2,7 @@ package models
 
 import "github.com/jinzhu/gorm"
 
+// NewServices sets up new services.
 func NewServices(connectionInfo string) (*Services, error) {
 	db, err := gorm.Open("postgres", connectionInfo)
 	if err != nil {
@@ -10,10 +11,32 @@ func NewServices(connectionInfo string) (*Services, error) {
 	db.LogMode(true)
 	return &Services{
 		User: NewUserService(db),
+		db:   db,
 	}, nil
 }
 
+// Services holds the various resources
 type Services struct {
 	Gallery GalleryService
 	User    UserService
+	db      *gorm.DB
+}
+
+// Close shuts down the database connection.
+func (s *Services) Close() error {
+	return s.db.Close()
+}
+
+// DestructiveReset drops all tables and rebuilds them.
+func (s *Services) DestructiveReset() error {
+	err := s.db.DropTableIfExists(&User{}, &Gallery{}).Error
+	if err != nil {
+		return err
+	}
+	return s.AutoMigrate()
+}
+
+// AutoMigrate will attempt to automatically migrate all tables.
+func (s *Services) AutoMigrate() error {
+	return s.db.AutoMigrate(&User{}, &Gallery{}).Error
 }
