80072e0f4ec83db1bfe1d206b6ae3e0bfa6ebf50
Andrew Brudnak <brudnak@icloud.com>
2020-02-23 23:26:12 -0700

    :framed_picture: Gallery services
    Implemented the gallery service and set it on the
    Services type.
    
    :neckbeard: unrelated info beyond this point :sparkles:
    
    :shipit:
    
    where to find me
    
    • :bird: https://twitter.com/Andrew_Brudnak
    • :bowtie: https://www.linkedin.com/in/andrew-brudnak/
    • :fox_face: https://gitlab.com/brudnak
    • :sake: https://bitbucket.org/brudnak/
    • :octocat: https://github.com/brudnak
    
    :blue_heart: just an aspiring gopher
    
    :lock: every commit i've ever made has been
    been signed with a gpg key
    
    :briefcase:
    https://www.midori-global.com/blog/2018/04/02/git-50-72-rule
    
    :anchor: have followed the 50/72 rule from day one
    
    :blue_heart: Main Go site
    https://go.dev/
    
    :blue_heart: Go Proverbs
    https://go-proverbs.github.io/
    
    :blue_heart: Zen of Go
    https://dave.cheney.net/2020/02/23/the-zen-of-go
    
    :sparkling_heart: Coding non-stop for Russell & Ruby
    :two_hearts: Ruby Genevieve
    :two_hearts: Russell James
    
    • :pray: bless the youtube algorithm
    • :headphones: https://youtu.be/waAlgFq9Xq8
    • :earth_americas: truth
    • :milky_way: https://youtu.be/9D05ej8u-gU

--- models/galleries.go
+++ models/galleries.go
@@ -10,10 +10,34 @@ type Gallery struct {
 	Title  string `gorm:"not_null"`
 }
 
-// GalleryService is our interface.
-type GalleryService interface{}
+type GalleryService interface {
+	GalleryDB
+}
 
-// GalleryDB interface for our data connection
 type GalleryDB interface {
 	Create(gallery *Gallery) error
+}
+
+func NewGalleryService(db *gorm.DB) GalleryService {
+	return &galleryService{
+		GalleryDB: &galleryValidator{&galleryGorm{db}},
+	}
+}
+
+type galleryService struct {
+	GalleryDB
+}
+
+type galleryValidator struct {
+	GalleryDB
+}
+
+var _ GalleryDB = &galleryGorm{}
+
+type galleryGorm struct {
+	db *gorm.DB
+}
+
+func (gg *galleryGorm) Create(gallery *Gallery) error {
+	return gg.db.Create(gallery).Error
 }
