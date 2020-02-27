24c7d8b15d567301fdd2b21f2bc03c8145ea1dc5
Andrew Brudnak <brudnak@icloud.com>
2020-02-27 12:58:24 -0700

    :fire: Updating the user model

--- models/users.go
+++ models/users.go
@@ -419,8 +419,8 @@ func (ug *userGorm) Delete(id uint) error {
 // first will query using the provided gorm.DB and it will
 // get the first item returned and place it into dst. If
 // nothing is found in the query, it will return ErrNotFound
-func first(db *gorm.DB, user *User) error {
-	err := db.First(user).Error
+func first(db *gorm.DB, dst interface{}) error {
+	err := db.First(dst).Error
 	if err == gorm.ErrRecordNotFound {
 		return ErrNotFound
 	}
