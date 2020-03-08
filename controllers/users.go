07e181ca73ed8b1825010af44e6fd0faebe49d28
Andrew Brudnak <brudnak@icloud.com>
2020-03-08 15:01:37 -0700

    :fire: Setting sign up alert

--- controllers/users.go
+++ controllers/users.go
@@ -72,7 +72,11 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/login", http.StatusFound)
 		return
 	}
-	http.Redirect(w, r, "/galleries", http.StatusFound)
+	alert := views.Alert{
+		Level:   views.AlertLvlSuccess,
+		Message: "Welcome to ChefHub.pw!",
+	}
+	views.RedirectAlert(w, r, "/galleries", http.StatusFound, alert)
 }
 
 // LoginForm handles our login form.
